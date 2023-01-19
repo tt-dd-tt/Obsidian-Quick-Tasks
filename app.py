49232364b028c666b3ea427c0b09cc6f3914cbf3
ttsstt <libor.riska@gmail.com>
2023-01-19 16:39:52 +0200

    writing from the center, larger font size

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from kivy.uix.button import Button
 class MyApp(App):
     def build(self):
         self.layout = FloatLayout()
-        self.text_input = TextInput(pos_hint={'center_x': 0.5, 'center_y': 0.5},size_hint=(.8,.1))
+        self.text_input = TextInput(pos_hint={'center_x': 0.5, 'center_y': 0.5},size_hint=(.8,.1), font_size=20, halign="center")
         self.button = Button(text="Confirm", pos_hint={'center_x': 0.5, 'center_y': 0.3},size_hint=(.3,.1))
         self.button.bind(on_release=self.write_to_file)
         self.layout.add_widget(self.text_input)
@@ -18,7 +18,7 @@ class MyApp(App):
         markdown_file = open("file.md", "a")
         markdown_file.write("- [ ] " + input_text + "\n")
         markdown_file.close()
-
+        self.text_input.text = ""
 if __name__ == "__main__":
     MyApp().run()
 
