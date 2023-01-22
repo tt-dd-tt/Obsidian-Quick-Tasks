afc8d44b5a9cc47c967bc23202d9e70b6dd61e65
ttsstt <libor.riska@gmail.com>
2023-01-22 17:08:10 +0200

    master

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
+from kivy.uix.label import Label
 
 class MyApp(App):
     def build(self):
@@ -15,10 +16,13 @@ class MyApp(App):
 
     def write_to_file(self, instance):
         input_text = self.text_input.text
+        if input_text == "":
+            self.text_input.text = "You didn't write anything!"
+            return
         markdown_file = open("file.md", "a")
         markdown_file.write("- [ ] " + input_text + "\n")
         markdown_file.close()
         self.text_input.text = ""
-if __name__ == "__main__":
-    MyApp().run()
 
+if __name__ == "__main__":
+    MyApp().run() 
