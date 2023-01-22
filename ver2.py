ca116b52fcccf6747d3502cd2e868ec957c18d93
ttsstt <libor.riska@gmail.com>
2023-01-22 22:48:47 +0200

    working on file-chooser and it's dropdown menu

---
+++ ver2.py
@@ -0,0 +1,46 @@
+from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.uix.filechooser import FileChooser
+
+
+class MyApp(App):
+    def build(self):
+        # create a layout and add a text input and button widget to it that binds to write_to_file function
+        self.layout = FloatLayout()
+        self.text_input = TextInput(pos_hint={'center_x': 0.5, 'center_y': 0.5},size_hint=(.8,.1), font_size=20, halign="center")
+        self.button = Button(text="Confirm", pos_hint={'center_x': 0.5, 'center_y': 0.3},size_hint=(.3,.1))
+        self.button.bind(on_release=self.write_to_file)
+        self.layout.add_widget(self.text_input)
+        self.layout.add_widget(self.button)
+        # create another button and widget that binds to file_chooser function
+        # this button will open a file chooser popup to choose a file to write to
+        self.file_chooser_button = Button(text="Choose File", pos_hint={'center_x': 0.5, 'center_y': 0.1},size_hint=(.3,.1))
+        self.file_chooser_button.bind(on_release=lambda x: self.file_chooser())
+        self.layout.add_widget(self.file_chooser_button)
+        
+        return self.layout
+# function to open file chooser popup and show files in the current directory where the app is running
+    def file_chooser(self, instance):
+        self.file_chooser = FileChooser()
+        self.file_chooser.bind(on_submit=lambda path, filename: self.handle_file_chooser_submit(path, filename))
+        self.layout.add_widget(self.file_chooser)
+
+# function to handle the selected file in a file chooser popup
+    def handle_file_chooser_submit(self, path, filename):
+        self.write_to_file(path, filename)
+
+    def write_to_file(self, path, filename):
+        input_text = self.text_input.text
+        file_path = path
+        if not file_path:
+            return None
+        markdown_file = open(file_path, "a")
+        markdown_file.write("- [ ] " + input_text + "\n")
+        markdown_file.close()
+        
+
+if __name__ == "__main__":
+    MyApp().run() 
+    
