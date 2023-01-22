0e4256c4a8ab14682b291e70c67771e1da0ef920
ttsstt <libor.riska@gmail.com>
2023-01-22 19:50:21 +0200

    maybe erase

--- app.py
+++ app.py
@@ -5,8 +5,18 @@ from kivy.uix.button import Button
 from kivy.uix.label import Label
 
 class MyApp(App):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.file_chosen = False
+        self.file_path = None
+
     def build(self):
         self.layout = FloatLayout()
+        self.file_chooser_button = Button(text='Select File', pos_hint={'center_x': 0.5, 'center_y': 0.7},size_hint=(.3,.1))
+        self.file_chooser_button.bind(on_release=self.file_chooser)
+        self.layout.add_widget(self.file_chooser_button)
+        self.file_chooser = FileChooser()
+        self.layout.add_widget(self.file_chooser)
         self.text_input = TextInput(pos_hint={'center_x': 0.5, 'center_y': 0.5},size_hint=(.8,.1), font_size=20, halign="center")
         self.button = Button(text="Confirm", pos_hint={'center_x': 0.5, 'center_y': 0.3},size_hint=(.3,.1))
         self.button.bind(on_release=self.write_to_file)
